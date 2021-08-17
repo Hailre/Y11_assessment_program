08eca57613f3059d649adaf13bcf9c575408f817
Hailre <reeceh@stu.otc.school.nz>
2021-08-17 15:03:43 +1200

    Tkinter Practice/Templating

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,23 @@
-
+import tkinter as tk
 from tkinter import *
+master = Tk()
+w = Canvas(master, width=40, height=60)
+w.pack()
 
-main = Tk()
+canvas_height =20
+canvas_width=200
 
-main.mainloop()
+y=int(canvas_height / 2)
+w.create_line(0, y, canvas_width, y)
+mainloop()
+
+r = tk.Tk()
+r.title('Flashcard Application')
+
+button = tk.Button(r, text='Exit Application', width=25, command=r.destroy)
+button.pack()
+r.mainloop()
+
+def menu_options():
+
+    return
