1c421f32b48c7df33802ca67386de606b1603161
Hailre <reeceh@stu.otc.school.nz>
2021-08-20 12:34:17 +1200

    Kivy Base Code

--- Kivy.py
+++ Kivy.py
@@ -1,5 +1,10 @@
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
